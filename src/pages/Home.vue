519a186a57f4697bad8f0fd1c481a8da6b09551a
weiya <372969383@qq.com>
2020-09-23 20:31:14 +0800

    :sparkles: (ui-component) 增加 UI 组件库

---
+++ src/pages/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    home
+
+    <v-card class="card" max-width="344" default>
+      <div>
+        <span class="number">
+          111
+        </span>
+        <span class="desc">
+          121212
+        </span>
+      </div>
+
+      <img src="../assets/img_coin@2x.png" />
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
